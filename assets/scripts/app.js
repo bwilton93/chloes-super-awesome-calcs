389ac743075980a1b3d1f58e6eb081b2d1ba3b4f
Barney <b.wilton1993@gmail.com>
2023-09-12 16:45:35 +0100

    Created function to convert date string into date object for calculations

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,31 @@
-const clientName = document.getElementById("name");
-const dateOfBirth = document.getElementById("date-of-birth");
-const joinDate = document.getElementById("date-joining-service");
-const leaveDate = document.getElementById("date-leaving-service");
-const finalPensionableSalary = document.getElementById(
+let clientName = document.getElementById("name");
+let dateOfBirth = document.getElementById("date-of-birth");
+let joinDate = document.getElementById("date-joining-service");
+let leaveDate = document.getElementById("date-leaving-service");
+let finalPensionableSalary = document.getElementById(
   "final-pensionable-salary"
 );
-const accrualNumerator = document.getElementById("accrual-rate-numerator");
-const accrualDenominator = document.getElementById("accrual-rate-denominator");
-const submitBtn = document.getElementById("submit-btn");
+let accrualNumerator = document.getElementById("accrual-rate-numerator");
+let accrualDenominator = document.getElementById("accrual-rate-denominator");
+let submitBtn = document.getElementById("submit-btn");
 
 const RunCalculations = () => {
-  logInfo();
+  dateOfBirth = convertToDate(dateOfBirth);
+  joinDate = convertToDate(joinDate);
 };
 
-const logInfo = () => {
+const convertToDate = (input) => {
+  return new Date(input.value);
+};
+
+var logInfo = function () {
   console.log(clientName.value);
   console.log(dateOfBirth.value);
-  console.log(joinDate.value);
+  console.log(joinDate);
+  console.log(typeof joinDate);
   console.log(leaveDate.value);
-  console.log(finalPensionableSalary.value)
-  console.log(`${accrualNumerator.value} / ${accrualDenominator.value}`)
+  console.log(finalPensionableSalary.value);
+  console.log(`${accrualNumerator.value} / ${accrualDenominator.value}`);
 };
 
 submitBtn.addEventListener("click", RunCalculations);
